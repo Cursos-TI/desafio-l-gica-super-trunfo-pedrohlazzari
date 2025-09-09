a778232939ba28a9f9f3d37839e9f61d6e599ab1
pedrohlazzari <pedrohlazzari@gmail.com>
2025-09-09 12:03:46 +0000

    (Nivel Aventureiro Concluido)

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    char estado1[50];
-    char estado2[50];
+    char pais1[50];
+    char pais2[50];
     char codigoest1[20];
     char codigoest2[20];
     char cidade1[50];
@@ -19,10 +19,11 @@ int main(){
     float pp1, pp2;
     float superpoder1, superpoder2;
     int resultado;
+    int opcao;
     
 
-    printf("Estado: \n");
-    scanf("%s", estado1);
+    printf("País: \n");
+    scanf("%s", pais1);
 
     printf("Código da carta: \n");
     scanf("%s", codigoest1);
@@ -50,7 +51,7 @@ int main(){
 
     printf("=-=-=-=-=-=-==-=-=-=-=-\n");
     printf("Carta 01\n");
-    printf("Estado: %s\n", estado1);
+    printf("Estado: %s\n", pais1);
     printf("Código da carta: %s\n", codigoest1);
     printf("Nome da cidade: %s\n", cidade1);
     printf("População: %d\n", populacao1);
@@ -63,8 +64,8 @@ int main(){
 
     printf("=-=-=-=-=-=-==-=-=-=-=-\n");
     printf("Carta 02\n");
-    printf("Estado: \n");
-    scanf("%s", estado2);
+    printf("País: \n");
+    scanf("%s", pais2);
 
     printf("Código da carta: \n");
     scanf("%s", codigoest2);
@@ -92,7 +93,7 @@ int main(){
 
     printf("=-=-=-=-=-=-==-=-=-=-=-\n");
     printf("Carta 02\n");
-    printf("Estado: %s\n", estado2);
+    printf("Estado: %s\n", pais2);
     printf("Código da carta: %s\n", codigoest2);
     printf("Nome da cidade: %s\n", cidade2);
     printf("População: %d\n", populacao2);
@@ -103,16 +104,107 @@ int main(){
     printf("PIB per capita: %.2f\n", pp2);
     printf("Super Poder: %.2f\n", superpoder2);
 
-   printf("=-=-=-Duelo das Cartas=-=-=-\n");
-   if(populacao1 > populacao2){
-    printf("POPULAÇÃO");
-    printf("Carta1 - %s\n", estado1);
-    printf("Carta2 - %s\n", estado2);
-    printf("RESULTADO: Carta1 (%s) Venceu!!!!", estado1);
-   }else{
-    printf("POPULAÇÃO");
-    printf("Carta1 - %s\n", estado1);
-    printf("Carta2 - %s\n", estado2);
-    printf("RESULTADO: Carta2 (%s) Venceu!!!!", estado2);
-   }
+    printf("-=-=-=Duelo das Cartas-=-=-=\n");
+    printf("ATRIBUTOS\n");
+    printf("1. População\n");
+    printf("2. Área\n");
+    printf("3. PIB\n");
+    printf("4. Número de otnos turísticos\n");
+    printf("5. Densidade Demográfica\n");
+    printf("Escolha qual atributo você irá comparar:");
+    scanf("%d", &opcao);
+
+    switch (opcao){
+    case 1:
+        if (populacao1 > populacao2)
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta1 Venceu\n");
+        }else if(populacao2 > populacao1)
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta2 Venceu\n");
+        }else
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta2 Empate\n");
+        }
+        break;
+        case 2:
+            if (area1 > area2)
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta1 Venceu\n");
+        }else if(area2 > area1)
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta2 Venceu\n");
+        }else
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta2 Empate\n");
+    }
+    break;
+    case 3:
+        if (pib1 > pib2)
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta1 Venceu\n");
+        }else if(pib2 > pib1)
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta2 Venceu\n");
+        }else
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta2 Empate\n");
+    }
+    break;
+    case 4:
+        if (pt1 > pt2)
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta1 Venceu\n");
+        }else if(pt2 > pt1)
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta2 Venceu\n");
+        }else
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta2 Empate\n");
+    }
+    break;
+    case 5:
+        if (dp1 > dp2)
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta1 Venceu\n");
+        }else if(dp2 > dp1)
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta2 Venceu\n");
+        }else
+        {
+            printf("Carta1: %s\n", pais1);
+            printf("Carta2: %s\n", pais2);
+            printf("Carta2 Empate\n");
+    }
+    break;
+
+}
 }
